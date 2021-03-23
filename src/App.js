ee2ee54809b142b18b477d0d483a92ef852760fe
Kishan2029 <63163246+Kishan2029@users.noreply.github.com>
2021-03-23 22:40:04 +0530

    Completing Project

--- src/App.js
+++ src/App.js
@@ -17,12 +17,21 @@ class App extends React.Component{
         console.log(error)
       })
   }
+ 
   render(){
     const {advice} =this.state;
     return(
-      <h1>{advice}</h1>
+      <div className="app">
+        <div className="card">
+            <h1 className="heading">{advice}</h1>
+            <button className="button" onClick={this.fetchAdvice}>
+              <span>GIVE ME ADVICE!</span>
+            </button>
+        </div>
+      </div>
     )
   }
 }
+
 
 export default App;
